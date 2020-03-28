08e96b816f395e2f3264c1efe669a82d0030ee99
Abhishek15725 <Abhishek15725@gmail.com>
2020-03-28 17:01:17 +0530

    initial commit

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -11,26 +11,101 @@ import problem3.node.Node;
 import java.util.*;
 
 public class MyPriorityQueue {
-        public static Comparator<Node> rollComparator = new Comparator<Node>() {
+    private Node front;
+    private Node rear;
+    private int size;
 
-            @Override
-            public int compare(Node r1, Node r2) {
-                return (int) (r1.getRoll() - r2.getRoll());
+    public MyPriorityQueue(){
+        front=null;
+        rear=null;
+        size=0;
+    }
+    public boolean isEmpty(){
+        boolean response=false;
+        if(front!=null || size!=0){
+            response=true;
+        }
+        return response;
+    }
+
+    public Node dequeue(){
+        Node response=null;
+        if(isEmpty()){
+            if(front.getNext()!=null){
+                response=new Node(front.getRoll());
+                front=front.getNext();
+                rear.setNext(front);
+                size--;
             }
-        };
-        public static void addDataToQueue(Queue<Node> studentPriorityQueue) {
-            for(int i=0; i<10; i++) {
-                int roll = i;
-                studentPriorityQueue.add(new Node(roll));
+            else {
+                response=new Node(front.getRoll());
+                front=null;
+                rear=null;
+                size--;
             }
         }
-        public static void pollDataFromQueue(Queue<Node> studentPriorityQueue) {
-            while(true){
-                Node stu = studentPriorityQueue.poll();
-                if(stu == null) break;
-                System.out.println("student roll="+stu.getRoll());
+        return response;
+    }
+
+    public void  enqueue(int data){
+        Node node=new Node(data);
+        if(front==null){
+            front=node;
+            rear=node;
+            size++;
+            System.out.println("Roll Entered successfully!");
+        }
+        else if(data<=front.getRoll()){
+            node.setNext(front);
+            front=node;
+            size++;
+            System.out.println("Roll Entered successfully");
+        }
+        else if(data>=rear.getRoll()){
+            node.setNext(front);
+            rear.setNext(node);
+            rear=node;
+            System.out.println("Roll Entered successfully");
+            size++;
+        }
+        else {
+            Node temp = front;
+            Node previous=temp;
+            while(temp.getNext().getNext()!=null){
+
+                if(data<=temp.getRoll()){
+                    previous.setNext(node);
+                    node.setNext(temp);
+                    size++;
+                    System.out.println("Roll Entered successfully");
+                    break;
+                }
+                previous=temp;
+                temp=temp.getNext();
             }
         }
+    }
+
+    //Traversing
+    public void traversing(){
+        if(front!=null) {
+            Node temp = front;
+            int temp1=size;
+            while (temp1!=0) {
+                System.out.print(temp.getRoll() + " ");
+                temp = temp.getNext();
+                temp1--;
+            }
+            System.out.println();
+        }
+        else {
+            System.out.println("sorry! Queue is empty.");
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
 
     }
 
